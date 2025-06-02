a08aa2b2e9eb60b3b8557cce1b56cd7e1e93888a
Roman Nebel <r@nebel.im>
2025-06-02 23:46:01 +0200

    Enhance DragAndDrop component by updating the description and formatting dragging status

--- src/pages/DragAndDrop.tsx
+++ src/pages/DragAndDrop.tsx
@@ -7,13 +7,16 @@ export default function DragAndDrop() {
   return (
     <div>
       <h1>Drag and Drop Example</h1>
-      <p>Drag and drop functionality will be implemented here.</p>
-      {isDragging && (
-        <p>
-          {source?.id || 'No source'} – {draggableObject?.id || 'None dragging'}{' '}
-          – {target?.id || 'No target'}
-        </p>
-      )}
+      <p>
+        Drag and drop functionality will be implemented here.{' '}
+        {isDragging && (
+          <i>
+            ({source?.id || 'No source'} –{' '}
+            {draggableObject?.id || 'None dragging'} –{' '}
+            {target?.id || 'No target'})
+          </i>
+        )}
+      </p>
       <DragArea1 dragAreaId="area1">
         <DragElement1 draggableId={'el'} dragSourceId={'area1'}></DragElement1>
       </DragArea1>
