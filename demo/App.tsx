d85a5534fde6368a00ba6907565edd7f620c3905
Roman Nebel <r@nebel.im>
2025-06-19 11:58:00 +0200

    Remove unused App.css and simplify App component structure

--- demo/App.tsx
+++ demo/App.tsx
@@ -1,4 +1,3 @@
-import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import DragAndDrop from './pages/DragAndDrop'
 import { DragAndDropProvider } from '../src/provider/DragAndDrop'
@@ -6,9 +5,7 @@ import { DragAndDropProvider } from '../src/provider/DragAndDrop'
 const App = () => {
   return (
     <DragAndDropProvider>
-      <Routes>
-        <Route path={'/'} element={<DragAndDrop />} />
-      </Routes>
+      <DragAndDrop />
     </DragAndDropProvider>
   )
 }
